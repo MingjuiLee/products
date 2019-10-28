ab658a636faad721008fb8b9af4cdb754d85eec4
李明叡 <leeray1208@limingruide-MacBook-Pro.local>
2019-10-28 00:03:55 -0700

    Add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,20 @@
-# read file, and then write in new data
-products = []	# big list
-with open('products.csv', 'r', encoding='utf-8') as f:
- 	for line in f:	# line is string
- 		if 'Product,Price' in line:
- 			continue	# 繼續
- 		name, price = line.strip().split(',')	# 先把換行符號\n去掉 在進行split(',') 用逗點,當作切割的標準
- 		products.append([name, price])	# only want to put real product and price in list, no header
-print(products)	
+import os	# operating systen
+
+# read file
+products = []	# whether or not finding this file, we will need this list
+if os.path.isfile('products.csv'):	# check file exist or not
+	print('yeah! Find this file!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+ 		for line in f:	# line is string
+ 			if 'Product,Price' in line:
+ 				continue	# 繼續
+ 			name, price = line.strip().split(',')	# 先把換行符號\n去掉 在進行split(',') 用逗點,當作切割的標準
+ 			products.append([name, price])	# only want to put real product and price in list, no header
+	print(products)	
+else:
+	print("Can't find file")
+
+# read file, and then write in new data	
 
 # let user input
 while True:
@@ -15,22 +23,12 @@ while True:
 		break
 	price = input("Please enter product's prices: ")	# string, not integer or float
 	price = int(price)	# note
-	#p = []
-	#p.append(name)	# small list
-	#p.append(price) # small list
-	#p = [name, price]	# faster
-	#products.append(p)
 	products.append([name, price])	# fastest
 print(products)
 
 # print all transaction record
 for p in products:
-	#print(p)	# print every small list
-	#print(p[0]) # print product name
 	print('The price of %s is %s' %(p[0], p[1]))
-
-#'abc' + '123' = 'abc123'
-#'abc' * 3 = 'abcabcabc'
 
 # write in file
 with open('products.csv', 'w', encoding='utf-8') as f:	# 'w': write, as f: use f to represent products.txt
