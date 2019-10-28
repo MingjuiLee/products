efe097e79309de751689edc9267f1501fefdfe97
李明叡 <leeray1208@limingruide-MacBook-Pro.local>
2019-10-27 23:47:30 -0700

    Add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# read file, and then write in new data
 products = []	# big list
+with open('products.csv', 'r', encoding='utf-8') as f:
+ 	for line in f:	# line is string
+ 		if 'Product,Price' in line:
+ 			continue	# 繼續
+ 		name, price = line.strip().split(',')	# 先把換行符號\n去掉 在進行split(',') 用逗點,當作切割的標準
+ 		products.append([name, price])	# only want to put real product and price in list, no header
+print(products)	
+
+# let user input
 while True:
 	name = input('Please enter product name: ')
 	if name == 'q':
@@ -13,6 +23,7 @@ while True:
 	products.append([name, price])	# fastest
 print(products)
 
+# print all transaction record
 for p in products:
 	#print(p)	# print every small list
 	#print(p[0]) # print product name
@@ -21,7 +32,8 @@ for p in products:
 #'abc' + '123' = 'abc123'
 #'abc' * 3 = 'abcabcabc'
 
-with open('product.csv', 'w', encoding='utf-8') as f:	# 'w': write, as f: use f to represent products.txt
+# write in file
+with open('products.csv', 'w', encoding='utf-8') as f:	# 'w': write, as f: use f to represent products.txt
 	f.write('Product,Price\n') # column name
 	for p in products:
 		#f.write(p[0] + ',' + p[1] + '\n')	# use + to combine name, price, ',', and line change, all string
